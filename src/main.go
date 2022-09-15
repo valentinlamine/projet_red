3ace701a7d1b9aad7a2b3c5f432b074a9dafb406
Valentin <valentin.lamine@ynov.com>
2022-09-15 17:21:54 +0200

    Création fichier affichage et nettoyage main.go

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ var inventaire database.Inventaire
 
 func main() {
 	setup_personnage()
-	Affichage_Personnage(p1)
+	database.Affichage_Personnage(p1)
 	carte.Init()
 	inventaire.Init()
 	Menu()
@@ -21,17 +21,17 @@ func main() {
 }
 
 func setup_personnage() {
-	Affichage("Création du personnage", []string{"Bienvenue dans le jeu de rôle !", "Pour commencer, vous devez créer votre personnage", "Choisissez un nom"})
+	database.Affichage("Création du personnage", []string{"Bienvenue dans le jeu de rôle !", "Pour commencer, vous devez créer votre personnage", "Choisissez un nom"})
 	var nom string
 	fmt.Scan(&nom)
 	//si le nom contient autre chose que des lettres, on demande de recommencer
 	for _, c := range nom {
 		if !strings.Contains("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", string(c)) {
-			Affichage("Erreur", []string{"Le nom ne peut contenir que des lettres", "Veuillez recommencer"})
+			database.Affichage("Erreur", []string{"Le nom ne peut contenir que des lettres", "Veuillez recommencer"})
 			fmt.Scan(&nom)
 		}
 	}
-	Affichage("Création du personnage", []string{"Bonjour jeune aventurier !", "Quelle est la classe de ton personnage ?", "il y a 4 classes : Guerrier, Chevalier, Pyromancien, Mendiant", "Pour choisir guerrier, tapez 1", "Pour choisir chevalier, tapez 2", "Pour choisir pyromancien, tapez 3", "Pour choisir mendiant, tapez 4"})
+	database.Affichage("Création du personnage", []string{"Bonjour jeune aventurier !", "Quelle est la classe de ton personnage ?", "il y a 4 classes : Guerrier, Chevalier, Pyromancien, Mendiant", "Pour choisir guerrier, tapez 1", "Pour choisir chevalier, tapez 2", "Pour choisir pyromancien, tapez 3", "Pour choisir mendiant, tapez 4"})
 	var classe int
 	fmt.Scan(&classe)
 	for classe < 1 || classe > 4 {
@@ -50,38 +50,8 @@ func setup_personnage() {
 	}
 }
 
-func Affichage(titre string, list []string) {
-	longest := 0
-	for _, s := range list {
-		if len(s) > longest {
-			longest = len(s)
-		}
-	}
-	if len(titre) > longest {
-		longest = len(titre)
-	}
-	fmt.Println(longest)
-	//affichage du haut de la boite
-	fmt.Print("╒", strings.Repeat("═", longest), "╕", "\n")
-	//affichage du titre
-	fmt.Print("│", titre, strings.Repeat(" ", longest-len(titre)+1), "│", "\n")
-	//affichage de la ligne
-	fmt.Print("├", strings.Repeat("─", longest), "┤", "\n")
-	//affichage des lignes de texte
-	for _, s := range list {
-		//fmt.Println(longest, len(s), longest-len(s))
-		fmt.Print("│", s, strings.Repeat(" ", longest-len(s)), "│", "\n")
-	}
-	//affichage du bas de la boite
-	fmt.Print("╘", strings.Repeat("═", longest), "╛", "\n")
-}
-
-func Affichage_Personnage(p database.Personnage) {
-	Affichage("Personnage", []string{"Nom : " + p.Nom, "Classe : " + p.Classe, "Niveau : " + string(p.Niveau), "Vie" + string(p.Pvact), "Pv max : " + string(p.Pvmax), "Vitalité : " + string(p.Vitalite), "Force : " + string(p.Force), "Dexterite : " + string(p.Dexterite), "Intelligence : " + string(p.Intelligence)})
-}
-
 func Menu() {
-	Affichage("Menu", []string{"Que voulez-vous faire ?", "1. Accéder aux statistiques du personnage", "2. Accéder à l'inventaire du personnage", "3. Quitter le jeu"})
+	database.Affichage("Menu", []string{"Que voulez-vous faire ?", "1. Accéder aux statistiques du personnage", "2. Accéder à l'inventaire du personnage", "3. Quitter le jeu"})
 	var choix int
 	fmt.Scan(&choix)
 	for choix < 1 || choix > 3 {
@@ -90,7 +60,7 @@ func Menu() {
 	}
 	switch choix {
 	case 1:
-		Affichage_Personnage(p1)
+		database.Affichage_Personnage(p1)
 	case 2:
 		fmt.Println("inventaire")
 	case 3:
