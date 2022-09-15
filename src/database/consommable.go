91fae4f645299356545dfc81ed831120404b4d1c
cotheo <theo.colliot@ynov.com>
2022-09-15 17:10:14 +0200

    Ajout BDD consommable

--- src/database/consommable.go
+++ src/database/consommable.go
@@ -17,9 +17,8 @@ type Consommable struct {
 	MultiLvlPoidsMax int
 }
 
-func (c *Consommable) InitIntern_Consommable(nom, classe string, prix, quantite, pvBonus, multiLvlFor, multiLvlDex, multiLvlInt, multiLvlPoidsMax int) {
+func (c *Consommable) InitIntern_Consommable(nom string, prix, quantite, pvBonus, multiLvlFor, multiLvlDex, multiLvlInt, multiLvlPoidsMax int) {
 	c.Nom = nom
-	c.Classe = classe
 	c.Prix = prix
 	c.Quantite = quantite
 	c.PvBonus = pvBonus
@@ -30,7 +29,15 @@ func (c *Consommable) InitIntern_Consommable(nom, classe string, prix, quantite,
 }
 
 func (c *Consommable) Init_Consommable(nom string) {
-	if nom == "Fiole d'Estus" {
-
+	if nom == "Fiole d'Estus" { //Potion de vie
+		c.InitIntern_Consommable(nom, 0, 3, 50, 0, 0, 0, 0)
+	} else if nom == "Résine de pin doré" { //Potion de force
+		c.InitIntern_Consommable(nom, 0, 3, 0, 2, 0, 0, 0)
+	} else if nom == "Résine de pin brulé" { //Potion de dextérité
+		c.InitIntern_Consommable(nom, 0, 3, 0, 0, 2, 0, 0)
+	} else if nom == "Résine de pin pourri" { //Potion d'intelligence
+		c.InitIntern_Consommable(nom, 0, 3, 0, 0, 0, 1, 0)
+	} else if nom == "Potion de poids max" {
+		c.InitIntern_Consommable(nom, 0, 3, 0, 0, 0, 0, 1)
 	}
 }
