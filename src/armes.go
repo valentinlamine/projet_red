f6d49a28ac882d4ca886b5c617d10b76342a68c6
cotheo <theo.colliot@ynov.com>
2022-09-15 08:55:23 +0200

    Appel d'Init selon nom d'arme

--- src/armes.go
+++ src/armes.go
@@ -22,6 +22,22 @@ func (a *Armes) Init(nom string, lvlMinFor, lvlMinDex, lvlMinInt, deg, poids int
 
 }
 
+func (a *Armes) Init2(nom string) {
+	if nom == "Dague" {
+		a.Init("Dague", 9, 11, 8, 20, 3)
+	} else if nom == "Claymore" {
+		a.Init("Claymore", 11, 9, 8, 50, 8)
+	} else if nom == "Rapière" {
+		a.Init("Rapière", 9, 12, 9, 35, 5)
+	} else if nom == "Uchigatana" {
+		a.Init("Uchigatana", 10, 14, 10, 40, 6)
+	} else if nom == "Bâton" {
+		a.Init("Bâton", 7, 7, 7, 15, 3)
+	} else if nom == "Hache queue de gargouille" {
+		a.Init("Hache queue de gargouille", 14, 10, 8, 60, 12)
+	}
+}
+
 func main() {
 	var Dague Armes
 	Dague.Init("Dague", 9, 11, 8, 20, 3)
